7febb08fdb8bb44fe7bcf89423668dc17a50278d
Roger Simms <roger.simms@gmail.com>
2023-03-23 12:36:43 +0000

    Add variable for env0 module registry

--- sample-3/variables.tf
+++ sample-3/variables.tf
@@ -3,3 +3,10 @@ variable "mybucket" {
   type        = string
   default     = "example-bucket"
 }
+
+variable "env0_registry_key" {
+  description = "Key+secret for env0 module registry"
+  type        = string
+  sensitive   = true
+  default     = ""
+}
